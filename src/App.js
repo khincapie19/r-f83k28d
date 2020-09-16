027de810ca1d48daf41d6bb7807419b4d130f6c2
Katherine <katherinehincapie19@hotmail.com>
2020-09-16 19:11:55 -0400

    Pruebas verificadas

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+    this.increment = this.increment.bind(this);
+  }
+  increment() {
+    this.setState(prevState=>({
+      count:prevState.count +1
+    }));
+  }
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.count}</span>
+        <button id='inc' onClick={this.increment}>Incrementa</button>
       </div>
     );
   }
